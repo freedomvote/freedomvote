2aa1d1d842ea1e94a33ee936604c151208c8a794
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-02-09 11:00:28 +0100

    Politicians can now be update

--- app/core/views.py
+++ app/core/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from core.models import Politician, Question, State, Party
 
 
 def politician_view(request, unique_url):
-    politician = Politician.objects.get(unique_url=unique_url)
+    politician = get_object_or_404(Politician, unique_url=unique_url)
     questions  = Question.objects.all().order_by('question_number')
     states     = State.objects.all().order_by('name')
     parties    = Party.objects.all().order_by('name')
+
+    if request.POST:
+        try:
+            politician.email = request.POST['email']
+            politician.future_plans = request.POST['future_plans']
+            politician.past_contributions = request.POST['past_contributions']
+            politician.is_member_of_parliament = bool(request.POST.get('is_member_of_parliament', False))
+            politician.party_other = request.POST['party_other']
+            try:
+                politician.image = request.FILES['image']
+            except:
+                pass
+            try:
+                politician.state = State.objects.get(pk=request.POST['state'])
+            except:
+                pass
+            try:
+                politician.party = State.objects.get(pk=request.POST['party'])
+            except:
+                pass
+            politician.save()
+
+        except:
+            pass
 
     return render(
         request,
@@ -18,7 +42,3 @@ def politician_view(request, unique_url):
             'parties'    : parties,
         }
     )
-
-def politician_update_view(request):
-    if request.POST:
-        pass
