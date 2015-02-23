827e803ec51a03fe5bfbe81389e4354f5dff7b5c
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-02-23 14:21:43 +0100

    Simplified AJAX post in politician view

--- app/static/politician.js
+++ app/static/politician.js
@@ -1,28 +1,6 @@
 jQuery(function ($){
   "use strict";
 
-  $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-      function getCookie(name) {
-        var cookieValue = null
-        if (document.cookie && document.cookie !== '') {
-          var cookies = document.cookie.split(';')
-          for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i])
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
-                break;
-            }
-          }
-        }
-        return cookieValue;
-      }
-      if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'))
-      }
-    },
-  })
-
   $('.slider').each(function() {
     var value = $(this).data('value')
     $(this).slider({
