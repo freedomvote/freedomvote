714da8adf54f4898fea015f6daf0b76ded4c990b
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-04-02 11:29:26 +0200

    Fixed piwik

--- app/freedomvote/settings.py
+++ app/freedomvote/settings.py
@@ -130,7 +130,10 @@ ROOT_URLCONF = 'freedomvote.urls'
 
 WSGI_APPLICATION = 'freedomvote.wsgi.application'
 
-INTERNAL_IPS = ['127.0.0.1',]
+INTERNAL_IPS = [
+    '0.0.0.0',
+    '127.0.0.1',
+]
 
 TEMPLATE_DIRS = (
     # The docs say it should be absolute path: BASE_DIR is precisely one.
