87b33e2b160836ec55795b852bcdebe3df27508d
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-06-22 11:32:19 +0200

    Added new scale with dots. refs #11816

--- app/core/static/js/profile.js
+++ app/core/static/js/profile.js
@@ -17,17 +17,11 @@ jQuery(function ($){
     createReadonlySlider($(this))
   })
 
-  $(window).resize(function() {
-    $('.slider-readonly').each(function() {
-      createReadonlySlider($(this))
-    })
-  })
-
   function createReadonlySlider(slider) {
     slider.empty()
-    var barWidth = slider.width() - (sliderOffset * 2)
-    var svg = '<svg width="'+slider.width()+'" height="30">' +
-              '<rect x="'+sliderOffset+'" y="10" rx="3" ry="3" height="10" width="'+barWidth+'"'+
+
+    var svg = '<svg viewBox="0 0 300 20" preserveAspectRatio="xMinYMin meet">' +
+              '<rect x="'+sliderOffset+'" y="5" rx="3" ry="3" height="10" width="'+(300-sliderOffset*2)+'"'+
               'style="fill:rgb(200,200,200);stroke-width:1;stroke:rgb(150,150,150)" />'
 
     var values = slider.data('slider-value')
@@ -35,16 +29,13 @@ jQuery(function ($){
     var texts  = slider.data('slider-text')
 
     for (var i = 0; i < values.length; i++) {
-      var value  = values[i]
-      var title  = titles[i]
-      var text   = texts[i]
       var ci     = i > (colors.length - 1) ? 0 : i
       var color  = colors[ci]
-      var posX   = sliderOffset + barWidth / 10 * value
-      var posY   = i % 2 == 0 ? 0 : 30
+      var posX   = sliderOffset + ((300 - sliderOffset * 2) / 10 * values[i])
+      var posY   = i % 2 == 0 ? -10 : 10
 
-      svg += '<polygon class="add-popover" data-title="'+title+'" data-text="'+text+'"'+
-             'points="'+posX+',15 '+(posX-sliderOffset)+','+posY+' '+(posX+sliderOffset)+','+posY+'" style="fill:'+color.fill+';stroke-width:1;stroke:'+color.stroke+'" />'
+      svg += '<path class="add-popover" data-title="'+titles[i]+'" data-text="'+texts[i]+'"'+
+             'd="M '+posX+' 10 l-7 '+posY+' l14 0 l-7 '+(-1*posY)+'" fill="'+color.fill+'" stroke-width="1" stroke="'+color.stroke+'" />'
     }
 
     slider.append(svg + '</svg>')
