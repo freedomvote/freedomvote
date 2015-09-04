52024ef92c73803e4cfb87a49a1a2406f3b0edf0
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-09-04 17:17:53 +0200

    use transaction

--- app/core/management/commands/politician_import.py
+++ app/core/management/commands/politician_import.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from core.models import Politician, State, Party
+from django.db import transaction
 import csv
 
 class Command(BaseCommand):
@@ -8,41 +9,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            rows = []
+            with transaction.atomic():
+                rows = []
 
-            with open(args[0], 'rt') as csvfile:
-                reader = csv.reader(csvfile)
-                count = 0
-                for row in reader:
-                    count += 1
-                    try:
-                        first_name = row[0]
-                        last_name  = row[1]
-                        email      = row[2]
-                        state      = State.objects.get(name=row[3])
-                        party      = Party.objects.get(shortname=row[4])
-                        user       = User.objects.get(username=row[5])
-                        member     = bool(row[6])
+                with open(args[0], 'rb') as csvfile:
+                    reader = csv.reader(csvfile)
+                    count = 0
+                    for row in reader:
+                        count += 1
+                        try:
+                            first_name = row[0]
+                            last_name  = row[1]
+                            email      = row[2]
+                            state      = State.objects.get(name=row[3])
+                            party      = Party.objects.get(shortname=row[4])
+                            user       = User.objects.get(username=row[5])
+                            member     = bool(row[6])
 
-                        p = Politician(
-                            user=user,
-                            first_name=first_name,
-                            last_name=last_name,
-                            email=email,
-                            state=state,
-                            party=party,
-                            is_member_of_parliament=member
-                        )
-                        p.save()
+                            p = Politician(
+                                user=user,
+                                first_name=first_name,
+                                last_name=last_name,
+                                email=email,
+                                state=state,
+                                party=party,
+                                is_member_of_parliament=member
+                            )
+                            p.save()
 
-                        rows.append([first_name, last_name, email, state.name, party.shortname, user.username, p.unique_url])
-                    except:
-                        print('Politician creation on line %d failed' % count)
-            with open('/tmp/export.csv', 'wb') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerows(rows)
+                            rows.append([first_name, last_name, email, state.name, party.shortname, user.username, p.unique_url])
+                        except:
+                            print('Politician creation on line %d failed' % count)
+                with open('/tmp/export.csv', 'wb') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerows(rows)
 
-                print('Export file is located in /tmp/export.csv')
+                    print('Export file is located in /tmp/export.csv')
 
         except:
             print('Could not parse target file')
