e4e418e01ac7636a28a9c7e6df2867b96a08177d
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-09-04 15:08:47 +0200

    Improved politician import

--- app/core/management/commands/politician_import.py
+++ app/core/management/commands/politician_import.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
-from core.models import Politician
+from core.models import Politician, State, Party
 import csv
 
 class Command(BaseCommand):
@@ -8,23 +8,39 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            rows = []
+
             with open(args[0], 'rb') as csvfile:
                 reader = csv.reader(csvfile)
+                count = 0
                 for row in reader:
+                    count += 1
                     try:
-                        user       = User.objects.get(username=row[0])
-                        first_name = row[1]
-                        last_name  = row[2]
-                        email      = row[3]
+                        first_name = row[0]
+                        last_name  = row[1]
+                        email      = row[2]
+                        state      = State.objects.get(name=row[3])
+                        party      = Party.objects.get(shortname=row[4])
+                        user       = User.objects.get(username=row[5])
 
                         p = Politician(
                             user=user,
                             first_name=first_name,
                             last_name=last_name,
-                            email=email
+                            email=email,
+                            state=state,
+                            party=party
                         )
                         p.save()
+
+                        rows.append([first_name, last_name, email, state.name, party.shortname, user.username, p.unique_url])
                     except:
-                        print('Politician creation failed')
+                        print('Politician creation on line %d failed' % count)
+            with open('/tmp/export.csv', 'wb') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(rows)
+
+                print('Export file is located in /tmp/export.csv')
+
         except:
             print('Could not parse target file')
