7c2aca292bb0e8263c760a8e43c85264589793cf
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-09-04 13:00:28 +0200

    Fixed access on property which doesn't exist

--- app/core/forms.py
+++ app/core/forms.py
@@ -13,8 +13,9 @@ class PoliticianForm(forms.ModelForm):
 
     def clean_image(self):
         image = self.cleaned_data.get('image')
+
         if image:
-            if image._size > 5*1024*1024:
+            if image.size > 5*1024*1024:
                 raise forms.ValidationError('Image file too large ( > 5MB )')
 
         return image
